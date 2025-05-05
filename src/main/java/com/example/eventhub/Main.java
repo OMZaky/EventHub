e83d37fbb1ba462e909d912cfb4e02deb797dbe3
Omar <24P0117@eng.asu.edu.eg>
2025-05-06 00:45:12 +0300

    Commit

--- src/main/java/com/example/eventhub/Main.java
+++ src/main/java/com/example/eventhub/Main.java
@@ -10,12 +10,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("APP");
+        stage.setWidth(700);
+        stage.setHeight(450);
+        stage.setMinHeight(400);
+        stage.setMinWidth(650);
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
