d8306742e6f2e9c7de3d3d0fe8e6a77c18fd0177
Omar <24P0117@eng.asu.edu.eg>
2025-05-06 00:08:04 +0300

    Commit

---
+++ src/main/java/com/example/eventhub/Main.java
@@ -0,0 +1,27 @@
+package com.example.eventhub;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a simple UI
+        Label label = new Label("Welcome to JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        // Set up the stage
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Launch the JavaFX application
+    }
+}
